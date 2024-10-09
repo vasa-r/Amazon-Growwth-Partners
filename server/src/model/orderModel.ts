5f13e50da345765db9a3437e6b47d335124d7732
VasaR <vasanthias52@gmail.com>
2024-10-09 09:13:03 +0530

    feat: random delivery date added in order model

--- server/src/model/orderModel.ts
+++ server/src/model/orderModel.ts
@@ -1,14 +1,37 @@
 import mongoose, { Schema, Document } from "mongoose";
+import { string } from "zod";
 
 export interface OrderType extends Document {
   user: mongoose.Types.ObjectId;
   address: mongoose.Types.ObjectId;
-  products: mongoose.Types.ObjectId[];
+  products: Array<{
+    id: mongoose.Types.ObjectId;
+    image: string;
+    price: string;
+    quantity: string;
+    title: string;
+  }>;
   totalPrice: number;
   paymentMode: string;
-  deliveryDate: Date;
-  orderedDate: Date;
+  deliveryDate: String;
+  orderedDate: String;
 }
+
+const formatDate = (date: Date): string => {
+  const day = String(date.getDate()).padStart(2, "0");
+  const month = String(date.getMonth() + 1).padStart(2, "0"); // Months are zero-based
+  const year = date.getFullYear();
+
+  return `${day}/${month}/${year}`;
+};
+
+const getRandomDeliveryDate = (daysFromToday: number): Date => {
+  const today = new Date();
+  const randomDays = Math.floor(Math.random() * daysFromToday) + 1; // Random number of days (1 to daysFromToday)
+  const deliveryDate = new Date(today);
+  deliveryDate.setDate(today.getDate() + randomDays);
+  return deliveryDate;
+};
 
 const orderSchema: Schema = new Schema<OrderType>(
   {
@@ -24,9 +47,28 @@ const orderSchema: Schema = new Schema<OrderType>(
     },
     products: [
       {
-        type: mongoose.Schema.Types.ObjectId,
-        ref: "Product",
-        required: true,
+        _id: false,
+        id: {
+          type: mongoose.Schema.Types.ObjectId,
+          ref: "Product",
+          required: true,
+        },
+        image: {
+          type: String,
+          required: true,
+        },
+        price: {
+          type: String,
+          required: true,
+        },
+        quantity: {
+          type: String,
+          required: true,
+        },
+        title: {
+          type: String,
+          required: true,
+        },
       },
     ],
     totalPrice: {
@@ -40,11 +82,12 @@ const orderSchema: Schema = new Schema<OrderType>(
     },
     deliveryDate: {
       type: Date,
+      default: () => getRandomDeliveryDate(15),
       required: true,
     },
     orderedDate: {
-      type: Date,
-      default: Date.now,
+      type: String,
+      default: formatDate(new Date()),
     },
   },
   { timestamps: true }
